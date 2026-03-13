934da34c24658c6dc48cd68d1f987cd0a3df58e1
Amar Singh <amar298976@gmail.com>
2026-03-13 20:34:44 +0530

    slider done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from './components/Header/Navbar'
-import HeroSection from './components/Header/HeroSection'
+import React from "react";
+import Navbar from "./components/Header/Navbar";
+import HeroSection from "./components/Header/HeroSection";
+import BodyPage from "./components/Pages/BodyPage";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <HeroSection/>
+      <Navbar />
+      <div className="px-4">
+        <HeroSection />
+        <BodyPage />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
