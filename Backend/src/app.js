cc4ea0214c38e4566387f1d42b4f7423899f47cc
Amar Singh <amar298976@gmail.com>
2026-05-09 20:11:39 +0530

    fix cors slash

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 app.use(cors({
     origin: ['http://localhost:5173', 
-    "https://ziva-care.vercel.app/"],
+    "https://ziva-care.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
 }))
